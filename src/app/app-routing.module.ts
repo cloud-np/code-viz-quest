08e97185dfc91b1cddd4627409946c233280f2af
cloud-np <cloud-np@outlook.com>
2023-05-07 22:15:00 +0300

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,99 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
+// import { UserService } from "./core/services/user.service";
+// import { ProfileComponent } from "./features/profile/profile.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    loadComponent: () =>
+      import("./features/home/home.component").then((m) => m.HomeComponent),
+  },
+//   {
+//     path: "login",
+//     loadComponent: () =>
+//       import("./core/auth/auth.component").then((m) => m.AuthComponent),
+//     canActivate: [
+//       () => inject(UserService).isAuthenticated.pipe(map((isAuth) => !isAuth)),
+//     ],
+//   },
+//   {
+//     path: "register",
+//     loadComponent: () =>
+//       import("./core/auth/auth.component").then((m) => m.AuthComponent),
+//     canActivate: [
+//       () => inject(UserService).isAuthenticated.pipe(map((isAuth) => !isAuth)),
+//     ],
+//   },
+//   {
+//     path: "settings",
+//     loadComponent: () =>
+//       import("./features/settings/settings.component").then(
+//         (m) => m.SettingsComponent
+//       ),
+//     canActivate: [() => inject(UserService).isAuthenticated],
+//   },
+//   {
+//     path: "profile",
+//     children: [
+//       {
+//         path: ":username",
+//         component: ProfileComponent,
+//         children: [
+//           {
+//             path: "",
+//             loadComponent: () =>
+//               import("./features/profile/profile-articles.component").then(
+//                 (m) => m.ProfileArticlesComponent
+//               ),
+//           },
+//           {
+//             path: "favorites",
+//             loadComponent: () =>
+//               import("./features/profile/profile-favorites.component").then(
+//                 (m) => m.ProfileFavoritesComponent
+//               ),
+//           },
+//         ],
+//       },
+//     ],
+//   },
+//   {
+//     path: "editor",
+//     children: [
+//       {
+//         path: "",
+//         loadComponent: () =>
+//           import("./features/editor/editor.component").then(
+//             (m) => m.EditorComponent
+//           ),
+//         canActivate: [() => inject(UserService).isAuthenticated],
+//       },
+//       {
+//         path: ":slug",
+//         loadComponent: () =>
+//           import("./features/editor/editor.component").then(
+//             (m) => m.EditorComponent
+//           ),
+//         canActivate: [() => inject(UserService).isAuthenticated],
+//       },
+//     ],
+//   },
+//   {
+//     path: "article/:slug",
+//     loadComponent: () =>
+//       import("./features/article/article.component").then(
+//         (m) => m.ArticleComponent
+//       ),
+//   },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {
+      preloadingStrategy: PreloadAllModules,
+    }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
