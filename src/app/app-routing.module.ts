9321bff61e814dd81a579176f739fa26f4e33c77
cloud-np <cloud-np@outlook.com>
2023-05-15 15:05:12 +0300

    fix(*): make home comp standalone

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,83 +9,83 @@ const routes: Routes = [
     loadComponent: () =>
       import("./features/home/home.component").then((m) => m.HomeComponent),
   },
-//   {
-//     path: "login",
-//     loadComponent: () =>
-//       import("./core/auth/auth.component").then((m) => m.AuthComponent),
-//     canActivate: [
-//       () => inject(UserService).isAuthenticated.pipe(map((isAuth) => !isAuth)),
-//     ],
-//   },
-//   {
-//     path: "register",
-//     loadComponent: () =>
-//       import("./core/auth/auth.component").then((m) => m.AuthComponent),
-//     canActivate: [
-//       () => inject(UserService).isAuthenticated.pipe(map((isAuth) => !isAuth)),
-//     ],
-//   },
-//   {
-//     path: "settings",
-//     loadComponent: () =>
-//       import("./features/settings/settings.component").then(
-//         (m) => m.SettingsComponent
-//       ),
-//     canActivate: [() => inject(UserService).isAuthenticated],
-//   },
-//   {
-//     path: "profile",
-//     children: [
-//       {
-//         path: ":username",
-//         component: ProfileComponent,
-//         children: [
-//           {
-//             path: "",
-//             loadComponent: () =>
-//               import("./features/profile/profile-articles.component").then(
-//                 (m) => m.ProfileArticlesComponent
-//               ),
-//           },
-//           {
-//             path: "favorites",
-//             loadComponent: () =>
-//               import("./features/profile/profile-favorites.component").then(
-//                 (m) => m.ProfileFavoritesComponent
-//               ),
-//           },
-//         ],
-//       },
-//     ],
-//   },
-//   {
-//     path: "editor",
-//     children: [
-//       {
-//         path: "",
-//         loadComponent: () =>
-//           import("./features/editor/editor.component").then(
-//             (m) => m.EditorComponent
-//           ),
-//         canActivate: [() => inject(UserService).isAuthenticated],
-//       },
-//       {
-//         path: ":slug",
-//         loadComponent: () =>
-//           import("./features/editor/editor.component").then(
-//             (m) => m.EditorComponent
-//           ),
-//         canActivate: [() => inject(UserService).isAuthenticated],
-//       },
-//     ],
-//   },
-//   {
-//     path: "article/:slug",
-//     loadComponent: () =>
-//       import("./features/article/article.component").then(
-//         (m) => m.ArticleComponent
-//       ),
-//   },
+  //   {
+  //     path: "login",
+  //     loadComponent: () =>
+  //       import("./core/auth/auth.component").then((m) => m.AuthComponent),
+  //     canActivate: [
+  //       () => inject(UserService).isAuthenticated.pipe(map((isAuth) => !isAuth)),
+  //     ],
+  //   },
+  //   {
+  //     path: "register",
+  //     loadComponent: () =>
+  //       import("./core/auth/auth.component").then((m) => m.AuthComponent),
+  //     canActivate: [
+  //       () => inject(UserService).isAuthenticated.pipe(map((isAuth) => !isAuth)),
+  //     ],
+  //   },
+  //   {
+  //     path: "settings",
+  //     loadComponent: () =>
+  //       import("./features/settings/settings.component").then(
+  //         (m) => m.SettingsComponent
+  //       ),
+  //     canActivate: [() => inject(UserService).isAuthenticated],
+  //   },
+  //   {
+  //     path: "profile",
+  //     children: [
+  //       {
+  //         path: ":username",
+  //         component: ProfileComponent,
+  //         children: [
+  //           {
+  //             path: "",
+  //             loadComponent: () =>
+  //               import("./features/profile/profile-articles.component").then(
+  //                 (m) => m.ProfileArticlesComponent
+  //               ),
+  //           },
+  //           {
+  //             path: "favorites",
+  //             loadComponent: () =>
+  //               import("./features/profile/profile-favorites.component").then(
+  //                 (m) => m.ProfileFavoritesComponent
+  //               ),
+  //           },
+  //         ],
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     path: "editor",
+  //     children: [
+  //       {
+  //         path: "",
+  //         loadComponent: () =>
+  //           import("./features/editor/editor.component").then(
+  //             (m) => m.EditorComponent
+  //           ),
+  //         canActivate: [() => inject(UserService).isAuthenticated],
+  //       },
+  //       {
+  //         path: ":slug",
+  //         loadComponent: () =>
+  //           import("./features/editor/editor.component").then(
+  //             (m) => m.EditorComponent
+  //           ),
+  //         canActivate: [() => inject(UserService).isAuthenticated],
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     path: "article/:slug",
+  //     loadComponent: () =>
+  //       import("./features/article/article.component").then(
+  //         (m) => m.ArticleComponent
+  //       ),
+  //   },
 ];
 
 @NgModule({
@@ -96,4 +96,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
